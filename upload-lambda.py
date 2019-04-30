747cdc0e86a92b691eb3e2b8fe31eeb48394447f
John Barnard <john.barnard991@gmail.com>
2019-04-30 01:19:10 -0400

    updated lambda upload script

--- upload-lambda.py
+++ upload-lambda.py
@@ -1,18 +1,16 @@
 import boto3
 from botocore.client import Config
-import BytesIO
 import zipfile
 
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('cloud.jbarnardportfolio.com')
 build_bucket = s3.Bucket('cloudbuild.jbarnardportfolio.com')
+portfolio_bucket = s3.Bucket('cloud.jbarnardportfolio.com')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
+with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).ACL().put(ACL='public-read')
+        target_bucket.upload_fileobj(obj, nm)
+        target_bucket.Object(nm).Acl().put(ACL='public-read')
